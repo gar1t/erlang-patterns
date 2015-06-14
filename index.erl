7a75e8321399d036fa7a0f875c836d765216ac64
Garrett Smith <g@rre.tt>
2015-06-14 14:48:32 +0200

    Custom tag to include lines from sample code

--- index.erl
+++ index.erl
@@ -22,3 +22,68 @@ site(Data) ->
 
 recent_patterns(Patterns) ->
     Patterns.
+
+%%===================================================================
+%% Custom tag - example
+%%
+%% Reads example source code for use in fenced markdown examples.
+%%
+%% For example:
+%%
+%% ```erlang
+%% {% example file="accumulator.erl" lines="5-10" %}
+%% ```
+%%===================================================================
+
+example(Vars) ->
+    read_file_lines(example_file(Vars), example_lines(Vars)).
+
+example_file(Vars) ->
+    Name = plist:value(file, Vars),
+    Path = example_file_path(Name),
+    handle_file_open(file:open(Path, [read]), Path).
+
+example_file_path(Name) ->
+    filename:join([lpad_session:root(), "examples", Name]).
+
+handle_file_open({ok, F}, _File) -> F;
+handle_file_open({error, Err}, File) ->
+    error({file_open, Err, File}).
+
+example_lines(Vars) ->
+    parse_lines(plist:value(lines, Vars)).
+
+parse_lines(Lines) ->
+    Pattern = "^([0-9]+)-([0-9]+)$",
+    Opts = [{capture, all_but_first, list}],
+    handle_lines_match(re:run(Lines, Pattern, Opts), Lines).
+
+handle_lines_match({match, [Start, Stop]}, _Lines) ->
+    {list_to_integer(Start), list_to_integer(Stop)};
+handle_lines_match(nomatch, Lines) ->
+    error({lines, Lines}).
+
+read_file_lines(In, {Start, Stop}) ->
+    file_seek(In, Start - 1),
+    Lines = file_acc_lines(In, (Stop - Start + 1)),
+    file:close(In),
+    Lines.
+
+file_seek(In, Count) -> file_seek(In, 0, Count).
+
+file_seek(In, Cur, Stop) when Cur < Stop ->
+    io:get_line(In, ""),
+    file_seek(In, Cur + 1, Stop);
+file_seek(_In, _Cur, _Stop) ->
+    ok.
+
+file_acc_lines(In, Count) -> file_acc_lines(In, 0, Count, []).
+
+file_acc_lines(In, Cur, Count, Acc) when Cur < Count ->
+    Line = io:get_line(In, ""),
+    file_acc_lines(In, Cur + 1, Count, [Line|Acc]);
+file_acc_lines(_In, _Cur, _Count, [Last|Rest]) ->
+    lists:reverse([strip_trailing_cr(Last)|Rest]).
+
+strip_trailing_cr(Line) ->
+    lists:sublist(Line, length(Line) - 1).
